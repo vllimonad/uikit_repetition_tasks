d24d0fd031526718ce7cd140e72d2137569788d7
Vlad Klunduk <klundukvlad40@gmail.com>
2024-08-05 16:35:28 +0200

    initial commit

---
+++ task_1/task_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  task_1
+//
+//  Created by Vlad Klunduk on 05/08/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
